8800d822bb93c8e0c3205e92bf292ebcdd6cfcc0
Fernand Pajot <fernand@epigram.me>
2017-04-23 20:22:59 -0700

    Added failure handling in cron job.

--- cron.sh
+++ cron.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 curl https://cronitor.link/E1KGCc/run -m 10 || true
 cd /root/theoutgroup/
-/usr/local/bin/python3.6 crawler.py
-/usr/local/bin/python3.6 articles.py
+(/usr/local/bin/python3.6 crawler.py && /usr/local/bin/python3.6 articles.py) || curl https://cronitor.link/E1KGCc/fail -m 10
 curl https://cronitor.link/E1KGCc/complete -m 10 || true
