c07c6f8d50dbc6f1b35249abd437e464750c4a02
Fernand Pajot <fernand@epigram.me>
2017-04-24 14:19:54 -0700

    Added caching of input links.

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ import asyncio
 from aiohttp import web
 import aiohttp_cors
 
-from helpers import loadj
+from crawler import PREFIX
+from helpers import loadj, writej
 import articles
 
 CORS = {
@@ -32,7 +33,15 @@ def clamp(scalar):
 
 async def similar(request):
     data = await request.json()
-    keywords = articles.get_article_info(data['url'])['keywords']
+    url = data['url'].split('?')[0]
+    # Keyword data can potentially be in two places.
+    if url in app['db']:
+        keywords = app['db'][url]['kws']
+    elif url in app['link_cache']:
+        keywords = app['link_cache'][url]
+    else:
+        keywords = articles.get_article_info(url)['keywords']
+        app['link_cache'][url] = keywords
     return web.json_response(get_articles(request.app, keywords, from_article=True))
 
 async def search(request):
@@ -92,7 +101,11 @@ def setup_routes(cors, app):
 
 def setup_data(app):
     app['db'] = None
+    app['link_cache'] = loadj(PREFIX + 'link_cache.json')
+    if app['link_cache'] is None:
+        app['link_cache'] = {}
     app['db_refresh'] = app.loop.create_task(schedule(app, refresh_db, 10*60))
+    app['flush_cache'] = app.loop.create_task(schedule(app, flush_cache, 15*60))
     app.on_cleanup.append(cancel_sched)
 
 async def schedule(app, task, interval):
@@ -104,8 +117,12 @@ def refresh_db(app):
     app['db'] = articles.load_keywords()
     build_indices(app)
 
+def flush_cache(app):
+    writej(app['link_cache'], PREFIX + 'link_cache.json')
+
 def cancel_sched(app):
     app['db_refresh'].cancel()
+    app['flush_cache'].cancel()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
