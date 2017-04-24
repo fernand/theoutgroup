85064d51aaf55568bcba3dbd565e7d006532bef5
Fernand Pajot <fernand@epigram.me>
2017-04-24 13:32:26 -0700

    Building indices.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import asyncio
 from aiohttp import web
 import aiohttp_cors
+import time
 
 from helpers import loadj
 import articles
@@ -15,7 +16,6 @@ CORS = {
     )
 }
 
-KEYWORDS = articles.load_keywords()
 POSITION = loadj('node_positions.json')
 MIN_ISEC_ARTICLE = 3
 MIN_USERS = 3
@@ -50,7 +50,8 @@ def get_articles(kws, from_article):
     else:
         min_isec = len(kws)
     matches = defaultdict(lambda:{})
-    for l, v in KEYWORDS.items():
+    t1 = time.time()
+    for l, v in app['db'].items():
         isec = len(v['kws'].intersection(kws))
         users = v['users']
         if isec >= min_isec and len(users) >= MIN_USERS:
@@ -61,6 +62,7 @@ def get_articles(kws, from_article):
             avg_pos = (avg_pos[0] - M[0], avg_pos[1] - M[1])
             scalar = U[0] * avg_pos[0] + U[1] * avg_pos[1]
             matches[l]['scalar'] = clamp(scalar)
+    print(time.time()-t1)
 
     ordered = sorted(matches.items(), key=lambda k_v: k_v[1]['scalar'])
     return [{
@@ -70,14 +72,38 @@ def get_articles(kws, from_article):
         'scalar': k_v[1]['scalar']
     } for k_v in ordered]
 
+def build_indices(app):
+    app['indices'] = defaultdict(lambda:set())
+    for l in app['db']:
+        for kw in app['db'][l]['kws']:
+            app['indices'][kw].add(l)
+
 def setup_routes(cors, app):
     for (endpoint, handler) in [('/similar', similar), ('/search', search)]:
         resource = cors.add(app.router.add_resource(endpoint))
         route = cors.add(resource.add_route('POST', handler), CORS)
+
+def setup_data(app):
+    app['db'] = None
+    app['db_refresh'] = app.loop.create_task(schedule(app, refresh_db, 10*60))
+    app.on_cleanup.append(cancel_sched)
+
+async def schedule(app, task, interval):
+    while True:
+        task(app)
+        await app.loop.create_task(asyncio.sleep(interval))
+
+def refresh_db(app):
+    app['db'] = articles.load_keywords()
+    app['indices'] = build_indices(app)
+
+def cancel_sched(app):
+    app['db_refresh'].cancel()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     app = web.Application(loop=loop)
     cors = aiohttp_cors.setup(app)
     setup_routes(cors, app)
+    setup_data(app)
     web.run_app(app, host='127.0.0.1', port=5000)
