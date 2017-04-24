9985be7a806969211eedfee04f66ba1093b97b9d
Fernand Pajot <fernand@epigram.me>
2017-04-24 15:50:22 -0700

    Host update.

--- server.py
+++ server.py
@@ -130,4 +130,4 @@ if __name__ == "__main__":
     cors = aiohttp_cors.setup(app)
     setup_routes(cors, app)
     setup_data(app)
-    web.run_app(app, host='127.0.0.1', port=5000)
+    web.run_app(app, port=5000)
