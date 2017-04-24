a28698d628fbdb580220f9fb9abc8fd015f5e11e
Fernand Pajot <fernand@epigram.me>
2017-04-23 17:47:02 -0700

    Updated cron job.

--- cron.sh
+++ cron.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 curl https://cronitor.link/E1KGCc/run -m 10 || true
 cd /root/theoutgroup/
-python3.6 crawler.py
+/usr/local/bin/python3.6 crawler.py
+/usr/local/bin/python3.6 articles.py
 curl https://cronitor.link/E1KGCc/complete -m 10 || true
